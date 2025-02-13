b7c3cdd45b90ffcbb99cbecda4f69621a27683a3
monosan77 <nogu.y-soccer.1o0706@ezweb.ne.jp>
2025-02-13 16:53:14 +0900

    front 画像のinputを追加

--- frontend/src/components/EditAccountForm/EditAccountForm.tsx
+++ frontend/src/components/EditAccountForm/EditAccountForm.tsx
@@ -5,6 +5,7 @@ import Email from '../AccountForm/Email';
 import TelNumber from '../AccountForm/TelNumber';
 import Buttons from '../AccountForm/AccountFormBtns';
 import useEditAccount from '@/hooks/account/useEditAccountForm';
+import AccountImage from '../AccountForm/AccountImage';
 
 type Prop = {
   accountData: AccountDataModel;
@@ -19,6 +20,7 @@ const EditAccountForm = ({ accountData }: Prop) => {
       <UserName register={register} errors={errors.userName?.message} />
       <Email register={register} errors={errors.email?.message} />
       <TelNumber register={register} errors={errors.tel?.message} />
+      <AccountImage register={register} errors={errors.image?.message} />
       <p className="h-6 text-center text-red-600 text-xs">{apiError}</p>
       <Buttons />
     </form>
